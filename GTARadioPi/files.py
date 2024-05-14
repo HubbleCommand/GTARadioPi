255b1aebe523f2c8f398fac2dc8980beab52a0b7
HubbleCommand <sasha.poirier@gmail.com>
2024-05-14 12:42:49 +0200

    more file utils

--- GTARadioPi/files.py
+++ GTARadioPi/files.py
@@ -2,11 +2,12 @@ import os
 from mutagen.wave import WAVE
 from mutagen.oggvorbis import OggVorbis
 import pygame
+from enum import Enum
+import json
 
 def count_files(src):
     count = len([name for name in os.listdir(src) if os.path.isfile(os.path.join(src, name))])
-    print("COUNT")
-    print(count)
+    print("count: ", count)
     return count
 
 def play_file(path):
@@ -18,8 +19,7 @@ def play_file(path):
 
 def get_track_object(path):
     extension = os.path.splitext(path)[1]
-    print("EXTENSION")
-    print(extension)
+    print("ext: ", extension)
     if extension == ".wav":
         print("Returning wav")
         return WAVE(path)
@@ -29,9 +29,7 @@ def get_track_object(path):
 
 def get_track_name(path):
     audio = WAVE(path)
-    print("Track Name")
-    print(audio.tags["TIT2"])
-    print(str(audio.tags["TIT2"]))
+    print("Track Name: ", audio.tags["TIT2"], " | ", str(audio.tags["TIT2"]))
     return str(audio.tags["TIT2"])
 
 def get_track_artist(path):
@@ -42,8 +40,37 @@ def get_track_duration(path):
     track_info = get_track_object(path)
     return track_info.info.length
 
-def count_files(src):
-    count = len([name for name in os.listdir(src) if os.path.isfile(os.path.join(src, name))])
-    print("COUNT")
-    print(count)
-    return count
+def read_station_name(path: str) -> str:
+    if not os.path.exists(path + "name.txt"):
+        return ""
+    file = open(path + "name.txt", 'r')
+    name = file.read()
+    file.close()
+    return name
+
+class Station(Enum):
+    UNSPLIT = 0
+    SPLIT = 1
+    TALKSHOW = 2
+#Station = Enum('Station', ['UNSPLIT', 'SPLIT', 'TALKSHOW'])
+def determine_station_type(path: str) -> Station|int:
+    #type is 0,1,2 (unsplit, split, talkshow)
+    if os.path.exists(path + "SRC.wav"):
+        return Station.UNSPLIT
+    
+    if os.path.exists(path + "SONGS/"):
+        return Station.SPLIT
+    
+    if os.path.exists(path + "MONO/"):
+        return Station.TALKSHOW
+    
+    return -1
+
+def load_path(path: str) -> dict:
+    if not os.path.exists(path) or not os.path.exists(os.path.join(path, "STATIONS")):
+        print("BAD")
+        return {}
+    
+    dirs = [dir for dir in os.listdir(path) if os.path.isdir(os.path.join(path, dir))]
+    print(dirs)
+    
